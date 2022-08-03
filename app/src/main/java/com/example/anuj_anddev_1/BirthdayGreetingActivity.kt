fba3404d0724429c22f89a81a8e4ecb9c1432164
aasiflm10 <94681493+aasiflm10@users.noreply.github.com>
2022-08-04 00:33:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/anuj_anddev_1/BirthdayGreetingActivity.kt
@@ -0,0 +1,23 @@
+package com.example.anuj_anddev_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_birthday_greeting.*
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+
+    //companion object is like a static variable
+    companion object {
+        const val NAME_EXTRA = "name_extra"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+
+        val name = intent.getStringExtra(BirthdayGreetingActivity.NAME_EXTRA)
+
+        birthdayGreeting.text = "Happy Birthday\n$name"
+
+
+    }
+}
